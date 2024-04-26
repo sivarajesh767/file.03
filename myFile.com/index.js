823feb4f3305c801feb21adec4ec365bdc7f3c9e
sivarajesh767 <sivarajesh767@gmail.com>
2024-04-26 06:24:41 +0000

    Code giving error

---
+++ myFile.com/index.js
@@ -0,0 +1,7 @@
+const addDate = require(date - fns / addDate)
+const dateX = date => {
+  const result = addDate(new Date(2020, 7, 22), date)
+
+  return `${result.getDate()}-${result.getmonth()}-${result.getYear()}`
+}
+console.log(dateX)
